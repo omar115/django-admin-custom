4957d89f48f4d7f82371dc5c3397ed002444e11e
omar115 <omarhasan115@gmail.com>
2021-10-06 15:08:35 +0600

    customizing admin panel...more to go

--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    price = models.IntegerField()
+    inventory = models.IntegerField()
+
+    # return the object name, by using the
+    # string representation of an object name in python
+    # we use __str__ method
+    def __str__(self) -> str:
+        return self.title
+    
+    # how to change the ordering of objects?
+    # we will Meta class to order the objects
+    class Meta:
+        ordering = ['title']
